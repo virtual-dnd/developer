d20cd65987f5d55c2ed617692346525f1ab5ee6b
Casey Baggz <caseybaggz@gmail.com>
2023-12-12 17:58:21 -0600

    feat: add initial configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,38 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify/functions";
+// import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify({
     edgeMiddleware: true
-  })
+  }),
+
+  integrations: [
+    // starlight({
+    //   title: 'Nurl Developers',
+    //   description: 'The official community and resource to build extensions for Nurl.',
+
+    //   locales: {
+    //     root: {
+    //       label: 'English',
+    //       lang: 'en',
+    //     }
+    //   },
+
+    //   social: {
+    //     discord: 'https://discord.gg/YCP8NgrKES',
+    //     github: 'https://github.com/nurl-inc',
+    //     linkedin: 'https://www.linkedin.com/company/nurl',
+    //     twitch: 'https://www.twitch.tv/nurl_app',
+    //     twitter: 'https://twitter.com/nurl_inc',
+    //   },
+
+    //   editLink: {
+    //     baseUrl: 'https://github.com/nurl-inc/developers/edit/main'
+    //   },
+    //   lastUpdated: true,
+    // })
+  ]
 });
